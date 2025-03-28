79c448be39dc86a25872734a72de347bfeabffb3
Andrey Karakhtanov <super.andrew100@yandex.ru>
2025-03-28 21:04:21 +0400

    Fix workflow and settings.

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -8,10 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY', get_random_secret_key())
 
-if os.getenv('DEBUG', 'False') == 'False':
-    DEBUG = False
-else:
-    DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = os.getenv('SERVERNAMES', '127.0.0.1 localhost').split(' ')
 
